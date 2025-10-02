28b265af05a8719381d9c8c5991dc1135b35a7f7
lukeosbourne123-dev <lukeosbourne123@outlook.com>
2025-10-02 14:56:52 -0600

    Branching Practice

--- branching_practice.py
+++ branching_practice.py
@@ -1,3 +1,4 @@
+'''
 age = int(input("Please enter your age \n"))
 of_legal_age = age >= 14
 
@@ -5,10 +6,21 @@ if of_legal_age:
     print("You are", age,"years old. You are allowed to drive")
 else:
     print("You are too young to drive")
+'''
+
+print("welcome to the Leap Year notifier")
+
+year = int(input("Please input year \n"))
+
+leap_year = year/4 
+
+if leap_year: 
+    print (year , "is a leap year")
 
 
 
- 
+
+
 
 
  
