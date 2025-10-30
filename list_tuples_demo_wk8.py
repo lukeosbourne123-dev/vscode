e1ea0a105c610f6bb2f333ed28acc04aefb5c2c7
lukeosbourne123-dev <lukeosbourne123@outlook.com>
2025-10-30 15:03:41 -0600

    updt notes

--- list_tuples_demo_wk8.py
+++ list_tuples_demo_wk8.py
@@ -52,13 +52,14 @@ days = ('mon', 'tue')
 print(days[0])
 #days[0] = 'thu' #mistake, tuples are immutable - unable to be changed
 print(days)
+print()
 
 days_ = list(days)
 days_.append('wed')
 print(days_)
 some_days = tuple(days_)
 print(some_days)
-
+print()
 #What is iterable? sequentially accessing elements within a collection or sequence, one by one - used in a for loop
 
 
@@ -75,8 +76,84 @@ del num1 # deletes it from memory
 print('num3',num3)
 num3.clear()
 print("num3",num3)
-
+print()
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 text = "Hello, how are you?" #string ---> iterable
 
+# in operator
+for char in text:
+    if char in vowels:
+        print(char, "is vowel")
+    else:
+        print(char, "is not vowel")
+
+print()
+
+#random.sort  - in place sort
+random = [9,0,80,25,8,6,-5,-10,1,2,4]
+random.sort()
+print(random)
+print()
+#another way to sort
+ordered = sorted(random)
+print(ordered)
+print()
+print()
+
+
+#copy data
+n1 = [1,2,3]
+n2 = n1
+n1[2] = 4
+n2[0] = 3
+print(n1, n2)
+
+m = n1.copy()
+print(m)
+print()
+m[0] = 7
+n1[1] = 9
+print(n1, m)
+
+text = "Hello world, how are you?"
+words = text.split(' ')
+print(words)
+print()
+words = ['John', 'Sam', 'Josh', 'Luquann']
+names = ", ".join(words)
+print("The list of students is:", names)
+print()
+print()
+#more functions
+print(max(random))
+print(min(random))
+print(sum(random))
+print(len(random))
+
+print(ordered)
+ordered.insert(5,6) #inserts "6" and index 5
+ordered.insert(6,5)
+print(ordered)
+print()
+print()
+
+#count function
+attendance = [True, False, True, True, False]
+print(attendance.count(True))
+print(ordered.index(6))
+print()
+
+print(random[:5])
+print(random[5:])
+print(random[2:6])
+
+gpa = []
+
+gpa.append(3.4)
+gpa.append(4.0)
+gpa.append(3.2)
+
+print(gpa)
+avg_gpa = sum(gpa)/len(gpa)
+print(f"{avg_gpa:>.2f}")
